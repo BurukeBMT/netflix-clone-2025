2b1373fdccd5b8f176a85aefca33db0fbd9adcf6
BurukeBMT <burukmaedot16@gmail.com>
2025-09-18 05:49:17 -0400

    netflix clone deployment

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import Home from './pages/Home/Home'
+
+
+
+
+function App() {
+
+  return (
+    <>
+    <div>
+      < Home/>
+    </div>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,21 @@
+import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
+import Banner from '../../components/Banner/Banner'
+import RowList from '../../components/Row/RowList/RowList'
+
+const Home = () => {
+  return (
+    <>
+      <div>
+        < Header/>
+        < Banner/>
+        <RowList/>
+        < Footer/>
+
+        
+      </div>
+    </>
+  )
+}
+
+export default Home
